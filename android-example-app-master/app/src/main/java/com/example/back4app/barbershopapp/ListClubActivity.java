74c17131d6a949af9d7d2fc248536476f887cf8d
HuynhNhatDuy <duyhuynhnhat@outlook.com>
2019-10-18 10:26:11 +0700

    Commit ListClub

---
+++ android-example-app-master/app/src/main/java/com/example/back4app/barbershopapp/ListClubActivity.java
@@ -0,0 +1,109 @@
+package com.example.back4app.barbershopapp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClubActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String url = " https://swd391fa2019.azurewebsites.net/api/Club/club";
+    private ProgressDialog pDialog;
+
+    private List<Model> modelList = new ArrayList<Model>();
+    private ListView listView;
+    private CustomAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_club);
+
+        listView = (ListView) findViewById(R.id.list_club);
+        adapter = new CustomAdapter(this, modelList);
+        listView.setAdapter(adapter);
+
+        pDialog = new ProgressDialog(this);
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+
+        // Creating volley request obj
+        JsonArrayRequest movieReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        hidePDialog();
+
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Model model = new Model();
+                                model.setTitle(obj.getString("clubId"));
+                                model.setCategory(obj.getString("clubName"));
+
+                                // adding model to movies array
+                                modelList.add(model);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidePDialog();
+
+            }
+        }
+        );
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListClubActivity.this)
+                        .setTitle("Club")
+                        .setMessage("What you want to do?");
+
+
+            }
+        });
+        // Adding request to request queue
+        App.getInstance().addToRequestQueue(movieReq);
+    }
+
+    private void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
+    }
+}
